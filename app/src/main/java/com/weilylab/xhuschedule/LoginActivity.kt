3a63bf944780d618054db208abce4594649c9e89
Mystery00 <mystery0dyl520@gmail.com>
2017-11-01 12:20:06 +0800

    加载验证码

--- app/src/main/java/com/weilylab/xhuschedule/LoginActivity.kt
+++ app/src/main/java/com/weilylab/xhuschedule/LoginActivity.kt
@@ -4,57 +4,78 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 
 import kotlinx.android.synthetic.main.activity_login.*
+import vip.mystery0.tools.hTTPok.HTTPok
+import vip.mystery0.tools.hTTPok.HTTPokResponse
+import vip.mystery0.tools.hTTPok.HTTPokResponseListener
+import vip.mystery0.tools.logs.Logs
+import java.io.File
 
 class LoginActivity : AppCompatActivity()
 {
+	companion object
+	{
+		private val TAG = "LoginActivity"
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_login)
-		password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
-			if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL)
-			{
-				return@OnEditorActionListener true
-			}
-			false
-		})
+
+		loadVcode()
 
 		username_sign_in_button.setOnClickListener { attemptLogin() }
 	}
 
+	private fun loadVcode()
+	{
+		Glide.with(this)
+				.load(getString(R.string.url_vcode))
+				.diskCacheStrategy(DiskCacheStrategy.NONE)
+				.into(vcode_image_view)
+	}
 
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
 	private fun attemptLogin()
 	{
 		username.error = null
 		password.error = null
+		vcode.error = null
 
 		val usernameStr = username.text.toString()
 		val passwordStr = password.text.toString()
+		val vcodeStr = vcode.text.toString()
 
 		var cancel = false
 		var focusView: View? = null
 
-		if (TextUtils.isEmpty(usernameStr))
+		when
 		{
-			username.error = getString(R.string.error_field_required)
-			focusView = username
-			cancel = true
-		}
-		else if (TextUtils.isEmpty(passwordStr))
-		{
-			password.error = getString(R.string.error_field_required)
-			focusView = password
-			cancel = true
+			TextUtils.isEmpty(usernameStr) ->
+			{
+				username.error = getString(R.string.error_field_required)
+				focusView = username
+				cancel = true
+			}
+			TextUtils.isEmpty(passwordStr) ->
+			{
+				password.error = getString(R.string.error_field_required)
+				focusView = password
+				cancel = true
+			}
+			TextUtils.isEmpty(vcodeStr) ->
+			{
+				vcode.error = getString(R.string.error_field_required)
+				focusView = vcode
+				cancel = true
+			}
 		}
 
 		if (cancel)
@@ -65,5 +86,10 @@ class LoginActivity : AppCompatActivity()
 		{
 //			showProgress(true)
 		}
+	}
+
+	private fun login()
+	{
+
 	}
 }
