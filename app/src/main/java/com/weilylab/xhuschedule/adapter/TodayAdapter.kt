b30eda307e8e1f4494d75475840f7b092684cf25
Mystery00 <mystery0dyl520@gmail.com>
2017-11-13 10:59:01 +0800

    画出课表名字的第一个字

--- app/src/main/java/com/weilylab/xhuschedule/adapter/TodayAdapter.kt
+++ app/src/main/java/com/weilylab/xhuschedule/adapter/TodayAdapter.kt
@@ -1,6 +1,6 @@
 package com.weilylab.xhuschedule.adapter
 
-import android.graphics.Color
+import android.graphics.*
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -25,10 +25,9 @@ class TodayAdapter(
 	override fun onBindViewHolder(holder: ViewHolder, position: Int)
 	{
 		val course = list[position]
-		if (course.color != "")
-			holder.img.setColorFilter(Color.parseColor(course.color))
-		else
-			holder.img.setColorFilter(Color.parseColor('#' + ScheduleHelper.getInstance().getRandomColor()))
+		if (course.color == "")
+			course.color = '#' + ScheduleHelper.getInstance().getRandomColor()
+		holder.img.setImageBitmap(drawImg(course))
 		holder.courseTimeTextView.text = course.time
 		val temp = course.name + " - " + course.teacher
 		holder.courseNameAndTeacherTextView.text = temp
@@ -47,5 +46,23 @@ class TodayAdapter(
 		var courseTimeTextView: TextView = itemView.findViewById(R.id.courseTimeTextView)
 		var courseNameAndTeacherTextView: TextView = itemView.findViewById(R.id.courseNameAndTeacherTextView)
 		var courseLocationTextView: TextView = itemView.findViewById(R.id.courseLocationTextView)
+	}
+
+	private fun drawImg(course: Course): Bitmap
+	{
+		val bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888)
+		val canvas = Canvas(bitmap)
+		val targetRect = Rect(0, 0, 200, 200)
+		canvas.drawColor(Color.parseColor(course.color))
+		val paint = Paint()
+		paint.color = Color.parseColor(course.color)
+		canvas.drawRect(targetRect, paint)
+		paint.color = Color.WHITE
+		paint.textSize = 120F
+		val fontMetrics = paint.fontMetrics
+		val baseline = (targetRect.bottom + targetRect.top - fontMetrics.bottom - fontMetrics.top) / 2
+		paint.textAlign = Paint.Align.CENTER
+		canvas.drawText(course.name.substring(0, 1), targetRect.centerX().toFloat(), baseline, paint)
+		return bitmap
 	}
 }
