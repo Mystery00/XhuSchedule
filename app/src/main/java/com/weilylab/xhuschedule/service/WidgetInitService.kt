5bc241b286d89c0700f54b78f920a5f561733dd0
Mystery00 <mystery0dyl520@gmail.com>
2017-12-22 01:25:51 +0800

    8.0服务新机制

--- app/src/main/java/com/weilylab/xhuschedule/service/WidgetInitService.kt
+++ app/src/main/java/com/weilylab/xhuschedule/service/WidgetInitService.kt
@@ -7,13 +7,35 @@
 
 package com.weilylab.xhuschedule.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
+import android.support.v4.app.NotificationCompat
+import com.weilylab.xhuschedule.R
 import com.weilylab.xhuschedule.util.widget.WidgetHelper
 import vip.mystery0.tools.logs.Logs
 
 class WidgetInitService : Service() {
+
+    private fun initChannelID(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val id = context.getString(R.string.notification_channel_id)
+            val name = context.getString(R.string.notification_channel_name)
+            val description = context.getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(id, name, importance)
+            mChannel.description = description
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.BLUE
+            mNotificationManager.createNotificationChannel(mChannel)
+        }
+    }
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -21,6 +43,11 @@ class WidgetInitService : Service() {
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Logs.i("TAG", "onStartCommand: ")
+        val builder = NotificationCompat.Builder(this, "Xhu Schedule")
+                .setAutoCancel(true)
+        val notification = builder.build()
+        startForeground(1, notification)
+
         Thread(Runnable {
             WidgetHelper.refreshWeekCourses(this)
             WidgetHelper.syncDayIndex()
