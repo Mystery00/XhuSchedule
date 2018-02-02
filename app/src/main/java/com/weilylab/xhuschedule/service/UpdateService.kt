a4e8a4789951b62d06064b216e9436819c0ec732
Mystery00 <mystery0dyl520@gmail.com>
2018-02-02 14:42:02 +0800

    add git ignore

--- app/src/main/java/com/weilylab/xhuschedule/service/UpdateService.kt
+++ app/src/main/java/com/weilylab/xhuschedule/service/UpdateService.kt
@@ -37,6 +37,7 @@ import android.app.IntentService
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import com.google.firebase.storage.FirebaseStorage
 import com.google.gson.Gson
 import com.weilylab.xhuschedule.R
 import com.weilylab.xhuschedule.classes.baseClass.Update
@@ -54,61 +55,66 @@ import java.io.InputStreamReader
 
 class UpdateService : IntentService("PhpService") {
     override fun onHandleIntent(intent: Intent?) {
-        ScheduleHelper.phpRetrofit
-                .create(PhpService::class.java)
-                .checkUpdateCall(getString(R.string.app_version_code).toInt())
-                .subscribeOn(Schedulers.newThread())
-                .unsubscribeOn(Schedulers.newThread())
-                .map { responseBody -> Gson().fromJson(InputStreamReader(responseBody.byteStream()), Update::class.java) }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : Observer<Update> {
-                    private lateinit var update: Update
+        val storage=FirebaseStorage.getInstance()
+        val storageReference=storage.reference
+        val apkReference=storageReference.child("apk")
+        val patchReference=storageReference.child("patch")
 
-                    override fun onSubscribe(d: Disposable) {
-                    }
-
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onComplete() {
-                        Logs.i("PhpService", "onComplete: " + update.message)
-                        if (update.code == 1 && update.version.versionCode != Settings.ignoreUpdate) {
-                            val version = update.version
-                            val title = getString(R.string.update_notification_title, getString(R.string.app_version_name), version.versionName)
-                            val bigText = getString(R.string.update_notification_big_text, version.updateLog)
-                            val builder = AlertDialog.Builder(APPActivityManager.appManager.currentActivity())
-                                    .setTitle(title)
-                                    .setMessage(bigText)
-                                    .setPositiveButton("${getString(R.string.action_download_apk)}(${FileUtil.FormatFileSize(version.apkSize)})", { _, _ ->
-                                        val downloadAPKIntent = Intent(this@UpdateService, DownloadService::class.java)
-                                        downloadAPKIntent.putExtra("type", "apk")
-                                        downloadAPKIntent.putExtra("fileName", version.versionAPK)
-                                        startService(downloadAPKIntent)
-                                    })
-                            if (version.lastVersion == getString(R.string.app_version_code).toInt())
-                                builder.setNegativeButton("${getString(R.string.action_download_patch)}(${FileUtil.FormatFileSize(version.patchSize)})", { _, _ ->
-                                    val downloadPatchIntent = Intent(this@UpdateService, DownloadService::class.java)
-                                    downloadPatchIntent.putExtra("type", "patch")
-                                    downloadPatchIntent.putExtra("fileName", version.lastVersionPatch)
-                                    startService(downloadPatchIntent)
-                                })
-                            if (version.isMust)
-                                builder.setOnCancelListener {
-                                    APPActivityManager.appManager.finishAllActivity()
-                                }
-                            else
-                                builder.setNeutralButton(R.string.action_download_cancel, { _, _ ->
-                                    Settings.ignoreUpdate = version.versionCode
-                                })
-                            builder.show()
-                        }
-                        stopSelf()
-                    }
-
-                    override fun onNext(update: Update) {
-                        this.update = update
-                    }
-                })
+//        ScheduleHelper.phpRetrofit
+//                .create(PhpService::class.java)
+//                .checkUpdateCall(getString(R.string.app_version_code).toInt())
+//                .subscribeOn(Schedulers.newThread())
+//                .unsubscribeOn(Schedulers.newThread())
+//                .map { responseBody -> Gson().fromJson(InputStreamReader(responseBody.byteStream()), Update::class.java) }
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(object : Observer<Update> {
+//                    private lateinit var update: Update
+//
+//                    override fun onSubscribe(d: Disposable) {
+//                    }
+//
+//                    override fun onError(e: Throwable) {
+//                        e.printStackTrace()
+//                    }
+//
+//                    override fun onComplete() {
+//                        Logs.i("PhpService", "onComplete: " + update.message)
+//                        if (update.code == 1 && update.version.versionCode != Settings.ignoreUpdate) {
+//                            val version = update.version
+//                            val title = getString(R.string.update_notification_title, getString(R.string.app_version_name), version.versionName)
+//                            val bigText = getString(R.string.update_notification_big_text, version.updateLog)
+//                            val builder = AlertDialog.Builder(APPActivityManager.appManager.currentActivity())
+//                                    .setTitle(title)
+//                                    .setMessage(bigText)
+//                                    .setPositiveButton("${getString(R.string.action_download_apk)}(${FileUtil.FormatFileSize(version.apkSize)})", { _, _ ->
+//                                        val downloadAPKIntent = Intent(this@UpdateService, DownloadService::class.java)
+//                                        downloadAPKIntent.putExtra("type", "apk")
+//                                        downloadAPKIntent.putExtra("fileName", version.versionAPK)
+//                                        startService(downloadAPKIntent)
+//                                    })
+//                            if (version.lastVersion == getString(R.string.app_version_code).toInt())
+//                                builder.setNegativeButton("${getString(R.string.action_download_patch)}(${FileUtil.FormatFileSize(version.patchSize)})", { _, _ ->
+//                                    val downloadPatchIntent = Intent(this@UpdateService, DownloadService::class.java)
+//                                    downloadPatchIntent.putExtra("type", "patch")
+//                                    downloadPatchIntent.putExtra("fileName", version.lastVersionPatch)
+//                                    startService(downloadPatchIntent)
+//                                })
+//                            if (version.isMust)
+//                                builder.setOnCancelListener {
+//                                    APPActivityManager.appManager.finishAllActivity()
+//                                }
+//                            else
+//                                builder.setNeutralButton(R.string.action_download_cancel, { _, _ ->
+//                                    Settings.ignoreUpdate = version.versionCode
+//                                })
+//                            builder.show()
+//                        }
+//                        stopSelf()
+//                    }
+//
+//                    override fun onNext(update: Update) {
+//                        this.update = update
+//                    }
+//                })
     }
 }
