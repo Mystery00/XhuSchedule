31900726d49d7f5bba8ae6305915f04899ba4287
Mystery00 <mystery0dyl520@gmail.com>
2018-02-21 22:29:44 +0800

    update settings

---
+++ app/src/main/java/com/weilylab/xhuschedule/fragment/DeveloperFragment.kt
@@ -0,0 +1,48 @@
+/*
+ * Created by Mystery0 on 18-2-21 下午10:05.
+ * Copyright (c) 2018. All Rights reserved.
+ *
+ *                    =====================================================
+ *                    =                                                   =
+ *                    =                       _oo0oo_                     =
+ *                    =                      o8888888o                    =
+ *                    =                      88" . "88                    =
+ *                    =                      (| -_- |)                    =
+ *                    =                      0\  =  /0                    =
+ *                    =                    ___/`---'\___                  =
+ *                    =                  .' \\|     |# '.                 =
+ *                    =                 / \\|||  :  |||# \                =
+ *                    =                / _||||| -:- |||||- \              =
+ *                    =               |   | \\\  -  #/ |   |              =
+ *                    =               | \_|  ''\---/''  |_/ |             =
+ *                    =               \  .-\__  '-'  ___/-. /             =
+ *                    =             ___'. .'  /--.--\  `. .'___           =
+ *                    =          ."" '<  `.___\_<|>_/___.' >' "".         =
+ *                    =         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       =
+ *                    =         \  \ `_.   \_ __\ /__ _/   .-` /  /       =
+ *                    =     =====`-.____`.___ \_____/___.-`___.-'=====    =
+ *                    =                       `=---='                     =
+ *                    =     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   =
+ *                    =                                                   =
+ *                    =               佛祖保佑         永无BUG              =
+ *                    =                                                   =
+ *                    =====================================================
+ *
+ * Last modified 18-2-21 下午9:12
+ */
+
+package com.weilylab.xhuschedule.fragment
+
+import android.os.Bundle
+import com.weilylab.xhuschedule.R
+
+/**
+ * Created by myste.
+ */
+class DeveloperFragment : BasePreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.preference_developer)
+    }
+}
---
+++ app/src/main/java/com/weilylab/xhuschedule/service/NotificationService.kt
@@ -0,0 +1,49 @@
+/*
+ * Created by Mystery0 on 18-2-21 下午9:25.
+ * Copyright (c) 2018. All Rights reserved.
+ *
+ *                    =====================================================
+ *                    =                                                   =
+ *                    =                       _oo0oo_                     =
+ *                    =                      o8888888o                    =
+ *                    =                      88" . "88                    =
+ *                    =                      (| -_- |)                    =
+ *                    =                      0\  =  /0                    =
+ *                    =                    ___/`---'\___                  =
+ *                    =                  .' \\|     |# '.                 =
+ *                    =                 / \\|||  :  |||# \                =
+ *                    =                / _||||| -:- |||||- \              =
+ *                    =               |   | \\\  -  #/ |   |              =
+ *                    =               | \_|  ''\---/''  |_/ |             =
+ *                    =               \  .-\__  '-'  ___/-. /             =
+ *                    =             ___'. .'  /--.--\  `. .'___           =
+ *                    =          ."" '<  `.___\_<|>_/___.' >' "".         =
+ *                    =         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       =
+ *                    =         \  \ `_.   \_ __\ /__ _/   .-` /  /       =
+ *                    =     =====`-.____`.___ \_____/___.-`___.-'=====    =
+ *                    =                       `=---='                     =
+ *                    =     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   =
+ *                    =                                                   =
+ *                    =               佛祖保佑         永无BUG              =
+ *                    =                                                   =
+ *                    =====================================================
+ *
+ * Last modified 18-2-21 下午9:25
+ */
+
+package com.weilylab.xhuschedule.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class NotificationService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+}
