46a5090eb347be567bc967189067c70c7ae2c579
Mystery00 <mystery0dyl520@gmail.com>
2018-02-21 21:23:45 +0800

    update copyright

--- app/src/main/java/com/weilylab/xhuschedule/view/TextViewUtils.java
+++ app/src/main/java/com/weilylab/xhuschedule/view/TextViewUtils.java
@@ -1,27 +1,37 @@
 /*
- * Created by Mystery0 on 18-2-16 上午10:30.
+ * Created by Mystery0 on 18-2-21 下午9:12.
  * Copyright (c) 2018. All Rights reserved.
  *
- * Last modified 18-2-16 上午10:30
+ *                    =====================================================
+ *                    =                                                   =
+ *                    =                       _oo0oo_                     =
+ *                    =                      o8888888o                    =
+ *                    =                      88" . "88                    =
+ *                    =                      (| -_- |)                    =
+ *                    =                      0\  =  /0                    =
+ *                    =                    ___/`---'\___                  =
+ *                    =                  .' \\|     |# '.                 =
+ *                    =                 / \\|||  :  |||# \                =
+ *                    =                / _||||| -:- |||||- \              =
+ *                    =               |   | \\\  -  #/ |   |              =
+ *                    =               | \_|  ''\---/''  |_/ |             =
+ *                    =               \  .-\__  '-'  ___/-. /             =
+ *                    =             ___'. .'  /--.--\  `. .'___           =
+ *                    =          ."" '<  `.___\_<|>_/___.' >' "".         =
+ *                    =         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       =
+ *                    =         \  \ `_.   \_ __\ /__ _/   .-` /  /       =
+ *                    =     =====`-.____`.___ \_____/___.-`___.-'=====    =
+ *                    =                       `=---='                     =
+ *                    =     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   =
+ *                    =                                                   =
+ *                    =               佛祖保佑         永无BUG              =
+ *                    =                                                   =
+ *                    =====================================================
+ *
+ * Last modified 18-2-21 下午9:11
  */
 
 package com.weilylab.xhuschedule.view;
-
-/*
- *    Copyright (C) 2017.  Aesean
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
