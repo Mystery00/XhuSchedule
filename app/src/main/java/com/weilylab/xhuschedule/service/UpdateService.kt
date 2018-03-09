39c2041006e5720b90c6d28e5fa03ec29b33d46e
Mystery0 <mystery0dyl520@gmail.com>
2018-03-09 16:33:13 +0800

    fix show dialog error: show update dialog but app exit

--- app/src/main/java/com/weilylab/xhuschedule/service/UpdateService.kt
+++ app/src/main/java/com/weilylab/xhuschedule/service/UpdateService.kt
@@ -53,66 +53,72 @@ import vip.mystery0.tools.fileUtil.FileUtil
 import java.io.InputStreamReader
 
 class UpdateService : IntentService("PhpService") {
-    override fun onCreate() {
-        super.onCreate()
-        val notification = NotificationCompat.Builder(this, Constants.NOTIFICATION_CHANNEL_ID_DEFAULT)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentText(getString(R.string.hint_foreground_notification))
-                .setAutoCancel(true)
-                .setPriority(NotificationManagerCompat.IMPORTANCE_NONE)
-                .build()
-        startForeground(0, notification)
-    }
-    override fun onHandleIntent(intent: Intent?) {
-        ScheduleHelper.phpRetrofit
-                .create(PhpService::class.java)
-                .checkVersion()
-                .subscribeOn(Schedulers.newThread())
-                .unsubscribeOn(Schedulers.newThread())
-                .map { responseBody -> Gson().fromJson(InputStreamReader(responseBody.byteStream()), Version::class.java) }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : DisposableObserver<Version>() {
-                    private lateinit var version: Version
-                    override fun onComplete() {
-                        if (version.versionCode > getString(R.string.app_version_code).toInt()) {
-                            val title = getString(R.string.dialog_update_title, getString(R.string.app_version_name), version.versionName)
-                            val text = getString(R.string.dialog_update_text, version.updateLog)
-                            val builder = AlertDialog.Builder(APPActivityManager.appManager.currentActivity())
-                                    .setTitle(title)
-                                    .setMessage(text)
-                                    .setPositiveButton("${getString(R.string.action_download_apk)}(${FileUtil.formatFileSize(version.apkSize)})", { _, _ ->
-                                        val downloadAPKIntent = Intent(this@UpdateService, DownloadService::class.java)
-                                        downloadAPKIntent.putExtra(Constants.INTENT_TAG_NAME_TYPE, Constants.DOWNLOAD_TYPE_APK)
-                                        downloadAPKIntent.putExtra(Constants.INTENT_TAG_NAME_QINIU_PATH, version.apkQiniuPath)
-                                        startService(downloadAPKIntent)
-                                    })
-                            if (version.lastVersionCode == getString(R.string.app_version_code).toInt())
-                                builder.setNegativeButton("${getString(R.string.action_download_patch)}(${FileUtil.formatFileSize(version.patchSize)})", { _, _ ->
-                                    val downloadPatchIntent = Intent(this@UpdateService, DownloadService::class.java)
-                                    downloadPatchIntent.putExtra(Constants.INTENT_TAG_NAME_TYPE, Constants.DOWNLOAD_TYPE_PATCH)
-                                    downloadPatchIntent.putExtra(Constants.INTENT_TAG_NAME_QINIU_PATH, version.patchQiniuPath)
-                                    startService(downloadPatchIntent)
-                                })
-                            if (version.must)
-                                builder.setOnCancelListener {
-                                    APPActivityManager.appManager.finishAllActivity()
-                                }
-                            else
-                                builder.setNeutralButton(R.string.action_download_cancel, { _, _ ->
-                                    Settings.ignoreUpdate = version.versionCode
-                                })
-                            builder.show()
-                        }
-                        stopSelf()
-                    }
+	override fun onCreate() {
+		super.onCreate()
+		val notification = NotificationCompat.Builder(this, Constants.NOTIFICATION_CHANNEL_ID_DEFAULT)
+				.setSmallIcon(R.mipmap.ic_launcher)
+				.setContentText(getString(R.string.hint_foreground_notification))
+				.setAutoCancel(true)
+				.setPriority(NotificationManagerCompat.IMPORTANCE_NONE)
+				.build()
+		startForeground(0, notification)
+	}
 
-                    override fun onNext(version: Version) {
-                        this.version = version
-                    }
+	override fun onHandleIntent(intent: Intent?) {
+		ScheduleHelper.phpRetrofit
+				.create(PhpService::class.java)
+				.checkVersion()
+				.subscribeOn(Schedulers.newThread())
+				.unsubscribeOn(Schedulers.newThread())
+				.map { responseBody -> Gson().fromJson(InputStreamReader(responseBody.byteStream()), Version::class.java) }
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(object : DisposableObserver<Version>() {
+					private lateinit var version: Version
+					override fun onComplete() {
+						if (version.versionCode > getString(R.string.app_version_code).toInt()) {
+							val title = getString(R.string.dialog_update_title, getString(R.string.app_version_name), version.versionName)
+							val text = getString(R.string.dialog_update_text, version.updateLog)
+							val currentActivity = APPActivityManager.appManager.currentActivity()
+							if (currentActivity == null) {
+								stopSelf()
+								return
+							}
+							val builder = AlertDialog.Builder(currentActivity)
+									.setTitle(title)
+									.setMessage(text)
+									.setPositiveButton("${getString(R.string.action_download_apk)}(${FileUtil.formatFileSize(version.apkSize)})", { _, _ ->
+										val downloadAPKIntent = Intent(this@UpdateService, DownloadService::class.java)
+										downloadAPKIntent.putExtra(Constants.INTENT_TAG_NAME_TYPE, Constants.DOWNLOAD_TYPE_APK)
+										downloadAPKIntent.putExtra(Constants.INTENT_TAG_NAME_QINIU_PATH, version.apkQiniuPath)
+										startService(downloadAPKIntent)
+									})
+							if (version.lastVersionCode == getString(R.string.app_version_code).toInt())
+								builder.setNegativeButton("${getString(R.string.action_download_patch)}(${FileUtil.formatFileSize(version.patchSize)})", { _, _ ->
+									val downloadPatchIntent = Intent(this@UpdateService, DownloadService::class.java)
+									downloadPatchIntent.putExtra(Constants.INTENT_TAG_NAME_TYPE, Constants.DOWNLOAD_TYPE_PATCH)
+									downloadPatchIntent.putExtra(Constants.INTENT_TAG_NAME_QINIU_PATH, version.patchQiniuPath)
+									startService(downloadPatchIntent)
+								})
+							if (version.must)
+								builder.setOnCancelListener {
+									APPActivityManager.appManager.finishAllActivity()
+								}
+							else
+								builder.setNeutralButton(R.string.action_download_cancel, { _, _ ->
+									Settings.ignoreUpdate = version.versionCode
+								})
+							builder.show()
+						}
+						stopSelf()
+					}
 
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                    }
-                })
-    }
+					override fun onNext(version: Version) {
+						this.version = version
+					}
+
+					override fun onError(e: Throwable) {
+						e.printStackTrace()
+					}
+				})
+	}
 }
--- app/src/main/java/com/weilylab/xhuschedule/util/APPActivityManager.kt
+++ app/src/main/java/com/weilylab/xhuschedule/util/APPActivityManager.kt
@@ -36,87 +36,88 @@ package com.weilylab.xhuschedule.util
 import android.app.Activity
 import java.util.*
 
-
 /**
  * Created by kun on 2016/7/12.
  * Activity管理类
  */
 class APPActivityManager private constructor() {
-    /**
-     * 添加Activity到堆栈
-     */
-    fun addActivity(activity: Activity) {
-        activityStack.add(activity)
-    }
+	/**
+	 * 添加Activity到堆栈
+	 */
+	fun addActivity(activity: Activity) {
+		activityStack.add(activity)
+	}
 
-    /**
-     * 获取当前Activity（堆栈中最后一个压入的）
-     */
-    fun currentActivity(): Activity {
-        return activityStack.lastElement()
-    }
+	/**
+	 * 获取当前Activity（堆栈中最后一个压入的）
+	 */
+	fun currentActivity(): Activity? {
+		if (!activityStack.empty())
+			return activityStack.lastElement()
+		return null
+	}
 
-    /**
-     * 获取倒数第二个Activity
-     */
-    fun lastLastActivity(): Activity? {
-        if (activityStack.size <= 2)
-            return null
-        else
-            return activityStack[activityStack.size - 2]
-    }
+	/**
+	 * 获取倒数第二个Activity
+	 */
+	fun lastLastActivity(): Activity? {
+		return if (activityStack.size <= 2)
+			null
+		else
+			activityStack[activityStack.size - 2]
+	}
 
-    /**
-     * 结束当前Activity（堆栈中最后一个压入的）
-     */
-    fun finishActivity() {
-        activityStack.lastElement()?.finish()
-    }
+	/**
+	 * 结束当前Activity（堆栈中最后一个压入的）
+	 */
+	fun finishActivity() {
+		activityStack.lastElement()?.finish()
+	}
 
-    /**
-     * 结束指定的Activity
-     */
-    fun finishActivity(activity: Activity) {
-        activityStack.remove(activity)
-        activity.finish()
-    }
+	/**
+	 * 结束指定的Activity
+	 */
+	fun finishActivity(activity: Activity) {
+		activityStack.remove(activity)
+		activity.finish()
+	}
 
-    /**
-     * 结束指定类名的Activity
-     */
-    fun finishActivity(cls: Class<*>) {
-        activityStack
-                .filter { it.javaClass == cls }
-                .forEach { finishActivity(it) }
-    }
+	/**
+	 * 结束指定类名的Activity
+	 */
+	fun finishActivity(cls: Class<*>) {
+		activityStack
+				.filter { it.javaClass == cls }
+				.forEach { finishActivity(it) }
+	}
 
-    /**
-     * 结束所有Activity
-     */
-    fun finishAllActivity() {
-        var i = 0
-        val size = activityStack.size
-        while (i < size) {
-            if (null != activityStack[i]) {
-                activityStack[i].finish()
-            }
-            i++
-        }
-        activityStack.clear()
-    }
+	/**
+	 * 结束所有Activity
+	 */
+	fun finishAllActivity() {
+		var i = 0
+		val size = activityStack.size
+		while (i < size) {
+			if (null != activityStack[i]) {
+				activityStack[i].finish()
+			}
+			i++
+		}
+		activityStack.clear()
+	}
 
-    companion object {
-        private var activityStack: Stack<Activity> = Stack()
-        private var instance: APPActivityManager? = null
-        /**
-         * 单一实例
-         */
-        val appManager: APPActivityManager
-            get() {
-                if (instance == null) {
-                    instance = APPActivityManager()
-                }
-                return instance!!
-            }
-    }
+	companion object {
+		private var activityStack: Stack<Activity> = Stack()
+		private var instance: APPActivityManager? = null
+		/**
+		 * 单一实例
+		 */
+		val appManager: APPActivityManager
+			get() {
+				if (instance == null) {
+					instance = APPActivityManager()
+				}
+				return instance!!
+			}
+	}
 }
