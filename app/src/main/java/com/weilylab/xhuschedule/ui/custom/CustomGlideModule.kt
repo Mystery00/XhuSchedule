0cf8cfe9e4ac15e7116b883ef13da01d86e13591
Mystery0 <mystery0dyl520@gmail.com>
2019-02-19 03:17:32 +0800

    修复加载校历失败，修复今日诗词对话框过长时无法滑动

---
+++ app/src/main/java/com/weilylab/xhuschedule/ui/custom/CustomGlideModule.kt
@@ -0,0 +1,23 @@
+package com.weilylab.xhuschedule.ui.custom
+
+import android.content.Context
+import androidx.annotation.NonNull
+import cc.shinichi.library.glide.sunfusheng.progress.ProgressManager
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.module.AppGlideModule
+import java.io.InputStream
+
+@GlideModule
+class CustomGlideModule : AppGlideModule() {
+	override fun registerComponents(@NonNull context: Context, @NonNull glide: Glide,
+									@NonNull registry: Registry) {
+		super.registerComponents(context, glide, registry)
+		// 替换底层网络框架为okhttp3，这步很重要！
+		// 如果您的app中已经存在了自定义的GlideModule，您只需要把这一行代码，添加到对应的重载方法中即可。
+		registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(ProgressManager.getOkHttpClient()))
+	}
+}
