9ee1491b4ab4877279bbfd7548298d2d21e35afe
Mystery0 <mystery0dyl520@gmail.com>
2019-03-20 23:01:27 +0800

    添加写入，查询，删除 用户数据的接口，暂时未调用

--- app/src/main/java/com/weilylab/xhuschedule/api/UserAPI.kt
+++ app/src/main/java/com/weilylab/xhuschedule/api/UserAPI.kt
@@ -14,4 +14,16 @@ interface UserAPI {
 	@FormUrlEncoded
 	@POST("/User/getInfo")
 	fun getInfo(@Field("username") username: String): Observable<ResponseBody>
+
+	@FormUrlEncoded
+	@POST("/User/setUserData")
+	fun setUserData(@Field("key") key: String, @Field("value") value: String, @Field("platform") platform: String = "Android"): Observable<ResponseBody>
+
+	@FormUrlEncoded
+	@POST("/User/getUserData")
+	fun getUserData(@Field("key") key: String, @Field("platform") platform: String = "Android"): Observable<ResponseBody>
+
+	@FormUrlEncoded
+	@POST("/User/delUserData")
+	fun delUserData(@Field("key") key: String, @Field("platform") platform: String = "Android"): Observable<ResponseBody>
 }
