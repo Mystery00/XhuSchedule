a1701a3f29dcfe4bfd6c6cc37bca133b912c1e7c
Mystery0 <mystery0dyl520@gmail.com>
2020-05-31 20:44:21 +0800

    fix: 更新版权信息

--- app/src/main/java/com/weilylab/xhuschedule/constant/ResponseCodeConstants.kt
+++ app/src/main/java/com/weilylab/xhuschedule/constant/ResponseCodeConstants.kt
@@ -1,34 +1,10 @@
 /*
- * Created by Mystery0 on 18-2-27 下午6:59.
- * Copyright (c) 2018. All Rights reserved.
+ *                     GNU GENERAL PUBLIC LICENSE
+ *                        Version 3, 29 June 2007
  *
- *                    =====================================================
- *                    =                                                   =
- *                    =                       _oo0oo_                     =
- *                    =                      o8888888o                    =
- *                    =                      88" . "88                    =
- *                    =                      (| -_- |)                    =
- *                    =                      0\  =  /0                    =
- *                    =                    ___/`---'\___                  =
- *                    =                  .' \\|     |# '.                 =
- *                    =                 / \\|||  :  |||# \                =
- *                    =                / _||||| -:- |||||- \              =
- *                    =               |   | \\\  -  #/ |   |              =
- *                    =               | \_|  ''\---/''  |_/ |             =
- *                    =               \  .-\__  '-'  ___/-. /             =
- *                    =             ___'. .'  /--.--\  `. .'___           =
- *                    =          ."" '<  `.___\_<|>_/___.' >' "".         =
- *                    =         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       =
- *                    =         \  \ `_.   \_ __\ /__ _/   .-` /  /       =
- *                    =     =====`-.____`.___ \_____/___.-`___.-'=====    =
- *                    =                       `=---='                     =
- *                    =     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   =
- *                    =                                                   =
- *                    =               佛祖保佑         永无BUG              =
- *                    =                                                   =
- *                    =====================================================
- *
- * Last modified 18-2-27 下午6:59
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
  */
 
 package com.weilylab.xhuschedule.constant
--- app/src/main/java/com/weilylab/xhuschedule/model/Download.kt
+++ app/src/main/java/com/weilylab/xhuschedule/model/Download.kt
@@ -1,34 +1,10 @@
 /*
- * Created by Mystery0 on 18-2-21 下午9:12.
- * Copyright (c) 2018. All Rights reserved.
+ *                     GNU GENERAL PUBLIC LICENSE
+ *                        Version 3, 29 June 2007
  *
- *                    =====================================================
- *                    =                                                   =
- *                    =                       _oo0oo_                     =
- *                    =                      o8888888o                    =
- *                    =                      88" . "88                    =
- *                    =                      (| -_- |)                    =
- *                    =                      0\  =  /0                    =
- *                    =                    ___/`---'\___                  =
- *                    =                  .' \\|     |# '.                 =
- *                    =                 / \\|||  :  |||# \                =
- *                    =                / _||||| -:- |||||- \              =
- *                    =               |   | \\\  -  #/ |   |              =
- *                    =               | \_|  ''\---/''  |_/ |             =
- *                    =               \  .-\__  '-'  ___/-. /             =
- *                    =             ___'. .'  /--.--\  `. .'___           =
- *                    =          ."" '<  `.___\_<|>_/___.' >' "".         =
- *                    =         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       =
- *                    =         \  \ `_.   \_ __\ /__ _/   .-` /  /       =
- *                    =     =====`-.____`.___ \_____/___.-`___.-'=====    =
- *                    =                       `=---='                     =
- *                    =     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   =
- *                    =                                                   =
- *                    =               佛祖保佑         永无BUG              =
- *                    =                                                   =
- *                    =====================================================
- *
- * Last modified 18-2-21 下午9:11
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
  */
 
 package com.weilylab.xhuschedule.model
